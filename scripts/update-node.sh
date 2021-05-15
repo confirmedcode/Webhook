211ef0d8a48a7de3193b3c5106d783ff4a650632
Johnny Lin <johnny@openlyoperated.org>
2021-05-15 13:39:47 -0700

    Node 12

--- scripts/update-node.sh
+++ scripts/update-node.sh
@@ -3,8 +3,10 @@
 set -x
 
 npm install -g n
-n 10
+n 12
 npm install -g npm@6
 
-# Install latest pm2
-npm install -g pm2
+# Rotate logs
+pm2 install pm2-logrotate
+pm2 set pm2-logrotate:max_size 10M
+pm2 set pm2-logrotate:retain 2
